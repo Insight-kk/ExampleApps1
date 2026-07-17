6e8b0d5e148ef3e738154bbae53b082c1379257e
TS-IKK <takeru-shirota@insightkk.co.jp>
2026-07-17 09:35:42 +0900

    AppSetting使用例追加

--- main_Logic.py
+++ main_Logic.py
@@ -6,9 +6,23 @@ from ikkToolKit import (XmlSettingBase,LogReporter,DynamicBindData,AsyncSequence
 )
 logger = LogReporter.setup(name="app", logDirctory="./", dumpLevel=logging.DEBUG, reportLevel=logging.INFO)
 
-
-
+###############################################################################
+class AppSetting(XmlSettingBase):
+    """ XMLの要素値利用して、動的にクラス変数を生成する
+    書き方  <公開プロパティ名>: (XMLのキー, 型, デフォルト値)
+    """
+    _PROPERTIES = {
+        "RecipeDir": ("RecipeDirectory", str, ""),
+        "SaveDir": ("StorageBaseDirectory", str, ""),
+        "ComPortNo": ("ComPortNo", str, ""),# 優先するCOMポート
+    }
+    def __init__(self):
+        super().__init__("setting.xml") #ファイル名
+        self.load()
+###############################################################################
 class MainLogic:
+    def __init__(self, debug_mode: bool = False):
+        self.setting = AppSetting()
     #------------------------------------------------------
     def start(self,root=None):  #必須
         self._tkRoot = root
@@ -26,8 +40,6 @@ class MainLogic:
 #
 ###############################################################################
 async def main():
-    # import sys
-    # debug_mode = "d" in sys.argv
     import msvcrt
     logic = MainLogic()
     logic.start()
@@ -43,7 +55,9 @@ async def main():
                     logic.function1()
                 if key == '2':
                     logic.function2()
-
+                elif key == 'e':
+                    print("\nEditing settings...")
+                    logic.setting.show_editor()  #スタンドアロンで設定エディタが開く
                 # elif key == '@': #有効なシリアルポート一覧を表示
                 #     print(ReaderCH340.PortLists())
             await asyncio.sleep(0.1)  # CPU使用率を抑える
